dada9d214bd904dbae0daad857b64f0e0fe089ad
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-08-28 14:28:25 +1000

    basic rust setup for backend dev

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::fs;
+use std::net::{ TcpListener, TcpStream };
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established");
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    if buffer.starts_with(get) {
+
+        let contents = fs::read_to_string("index.html").unwrap();
+        // println!("{contents}");
+    
+        // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    
+        // let response = "HTTP/1.1 200 OK\r\n\r\n";
+        let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
+    
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+
 }
